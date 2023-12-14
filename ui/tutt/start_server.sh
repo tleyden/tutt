d340a0b8234460487825c5e555806e976204589a
Traun Leyden <traun.leyden@gmail.com>
2023-12-14 12:12:30 +0100

    If the dataset file doesn't exist, create it so the UI doesn't show a confusing error message

--- ui/tutt/start_server.sh
+++ ui/tutt/start_server.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # Does the symlink in public directory exist?  If not, create it
 if [ ! -L public/dataset ]; then
@@ -5,4 +6,18 @@ if [ ! -L public/dataset ]; then
     ln -s ../../../dataset public/dataset
 fi
 
+# Define the dataset file path
+filePath="public/dataset/dataset.json"
+
+# If the dataset file doesn't exist, create it so the UI doesn't show
+# a confusing error message
+if [ ! -f "$filePath" ]; then
+    # File does not exist, create the directory if it doesn't exist
+    mkdir -p "$(dirname "$filePath")"
+    
+    # Create the file with the initial content
+    echo '{"entries": []}' > "$filePath"
+fi
+
+# Run the server in dev mode
 npm run dev
